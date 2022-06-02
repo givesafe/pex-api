412cc9ba9e61f3401a61712909c6d46fc193cca8
Danny Sperry <danny.sperry@gmail.com>
2022-06-02 16:23:08 -0700

    0.2.7 -- requiring all pex files in glob

--- lib/pex_api.rb
+++ lib/pex_api.rb
@@ -3,22 +3,7 @@
 require_relative "pex_api/version"
 require_relative "pex_api/configuration"
 
-require_relative "pex_api/card/activate"
-require_relative "pex_api/card/create_async"
-require_relative "pex_api/card/fund"
-require_relative "pex_api/card/set_pin"
-require_relative "pex_api/card/terminate"
-require_relative "pex_api/card/zero_balance"
-
-require_relative "pex_api/details/account_details"
-require_relative "pex_api/details/transaction_details"
-
-require_relative "pex_api/webhook/auth_token"
-
-require_relative "pex_api/client/token"
-require_relative "pex_api/client/basic"
-
-require_relative "pex_api/group/get"
+Dir["pex_api/**/*.rb"].each {|file| require file }
 
 module PexApi
   class Error < StandardError; end
