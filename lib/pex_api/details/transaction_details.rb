b911b90ad74c9e74c5dee514e3be27d809bb6eb6
Monty Lennie <montylennie@gmail.com>
2023-08-16 17:44:29 -0600

    get transactionDetails fetching working

--- lib/pex_api/details/transaction_details.rb
+++ lib/pex_api/details/transaction_details.rb
@@ -9,19 +9,18 @@ module PexApi
       #
       # THIS IS PROBABLY NOT NECESSARY AND SHOULD JUST BE MANAGED BY THE BACKEND TRANSACTION HISTORY.
       # IF CHARGES OCCUR ON THIS CARD THAT DONT OCCUR ON THE BACKEND THEN THIS SHOULD BE USED IN A BACKEND CRON JOB TO UPDATE TRANSACTION HISTORY
-      def self.call(card_account_id, *args)
+      def self.call(card_account_id, **args)
         _path = "Details/TransactionDetails/#{card_account_id}"
-        
+
         _params = {
-          StartDate: args.fetch(:start_date, DateTime.now.prev_month.to_s),
-          EndDate: args.fetch(:end_date, DateTime.now.to_s),
-          IncludePendings: (args.fetch(:include_pendings, false) ? 1 : 0),
-          IncludeDeclines: (args.fetch(:include_declines, false) ? 1 : 0)
+          "StartDate" => args.fetch(:start_date, DateTime.now.prev_month.to_s),
+          "EndDate" => args.fetch(:end_date, DateTime.now.to_s),
+          "IncludePendings" => args.fetch(:include_pendings, false),
+          "IncludeDeclines" => args.fetch(:include_declines, false)
         }
-        
-        ::PexApi::Client::Token.new.get(_path, _params)
+
+        ::PexApi::Client::Token.new.get(_path, params: _params)
       end
     end
   end
 end
-
